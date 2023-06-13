aac3397bb9ad71de9c75b477313c5cd63dc20e4c
deschi0000 <79345657+deschi0000@users.noreply.github.com>
2023-06-13 18:18:40 +0800

    add mongoose

--- config/app.js
+++ config/app.js
@@ -4,11 +4,24 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
+// Database setup
+let mongoose = require('mongoose');
+let DB = require('./db');
+
 let indexRouter = require('../routes/index');
 let usersRouter = require('../routes/users'); 
 
-let app = express();
+// Point mongoose to the DB URI
+mongoose.connect(DB.URI);
 
+let mongoDB = mongoose.connection;
+mongoDB.on('error', console.error.bind(console, 'Connection Error:'));
+mongoDB.once('open', () => {
+  console.log('Connected to MongoDB...');
+})
+
+
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, '../views'));
