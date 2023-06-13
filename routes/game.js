9af3c4e4cdfa3740a626498f7f6f87fc8c64f842
deschi0000 <79345657+deschi0000@users.noreply.github.com>
2023-06-13 18:26:32 +0800

    make game route

---
+++ routes/game.js
@@ -0,0 +1,18 @@
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// Connect to our game model
+let Contact = require('../modules/contact');
+
+// Get Route for the Game List page
+router.get('/', async (req, res, next) => {
+    try {
+        let contactList = await Contact.find();
+        console.log(contactList);
+    } catch (err){
+        console.log(err);
+    }
+});
+
+module.exports = router;
