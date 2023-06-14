926ff4c806357da42d7519fcb5cb507ed6183f51
deschi0000 <79345657+deschi0000@users.noreply.github.com>
2023-06-14 15:35:36 +0800

    link new controllers / clean routes

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -2,113 +2,25 @@ let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
 
-// Connect to our game model
-let Contact = require('../models/contact');
+// Get the Contacts Controller
+let contactController = require('../controllers/contact');
 
 // Get Route for the Game List page --  READ OPERATION
-router.get('/', async (req, res, next) => {
-    try {
-        let contactList = await Contact.find();
-        // console.log(contactList);
-
-        res.render('contacts/list', {
-            title: 'Contacts',
-            ContactList: contactList
-        });
-    } catch (err){
-        console.log(err);
-    }
-});
+router.get('/', contactController.displayContactList);
 
 // Get Route for the Add Page  --  CREATE OPERATION
-router.get('/add', async (req, res, next) => {
-    try {
-        res.render('contacts/add', {title: 'Add a Contact'});
-    } catch (err){
-        console.log(err);
-    }
-});
-
-
+router.get('/add', contactController.displayAddPage);
 
 // Post Route for processing the Add page  --  CREATE OPERATION
-router.post('/add', async (req, res, next) => {
-
-    let newContact = new Contact({
-        "lastname":req.body.lastname,
-        "firstname":req.body.firstname,
-        "contactnumber":req.body.contactnumber,
-        "email":req.body.email
-    })
-
-    try {
-        await newContact.save();
-        res.redirect('/contact-list');
-    } catch(err) {
-        console.log(err);
-        res.status(500).send(err);
-    }
-});
-
+router.post('/add', contactController.processAddPage);
 
 // Get Route for displaying Edit Page  --  UPDATE OPERATION
-router.get('/edit/:id', async (req, res, next) => {
-    
-    let id = req.params.id;
-
-    try {
-        let contactToEdit = await Contact.findById(id);
-        res.render('contacts/edit', {title: 'Edit Contact', contact: contactToEdit});
-    } catch(err) {
-        console.log(err);
-        res.status(500).send(err);
-    }
-});
-
+router.get('/edit/:id', contactController.displayEditPage);
 
 // Post Route for processing the Edit page  --  UPDATE OPERATION
-router.post('/edit/:id', async (req, res, next) => {
-    
-    let id = req.params.id;
-
-    let updatedContact = {
-        "lastname":req.body.lastname,
-        "firstname":req.body.firstname,
-        "contactnumber":req.body.contactnumber,
-        "email":req.body.email
-    }
-
-    try {
-        await Contact.updateOne({_id: id}, updatedContact);
-        res.redirect('/contact-list')
-    } catch(err) {
-        console.log(err);
-        res.status(500).send(err);
-    }
-});
-
+router.post('/edit/:id', contactController.processEditPage);
 
 // Get to perform Deletion  --  DELETE OPERATION
-router.get('/delete/:id', async (req, res, next) => {
-    let id = req.params.id;
-
-    try {
-        await Contact.findByIdAndRemove(id);
-        res.redirect('/contact-list');
-    } catch (err) {
-        console.log(err);
-        res.status(500).send(err);
-    }
-});
-
-
-
-
-
-
-
-
-
-
+router.get('/delete/:id', contactController.performDelete);
 
 module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,71 +2,28 @@
 let express = require('express');
 let router = express.Router();
 
+// Get the Controller
+let indexController = require('../controllers/index');
+  
 /* GET Home page. */
-router.get('/', (req, res, next) =>  {
-   
-  // If there is a param in the query, then display thank you message
-  if (req.query.param1){
-    let param1 = req.query.param1;
-    console.log(param1);
-
-    res.render('index', { title: 'Home', message: 'Thank You'});
-
-  }
-  res.render('index', { title: 'Home', message: ""});
-
-});
+router.get('/', indexController.displayHomePage);
 
 /* GET Home page. */
-router.get('/home', (req, res, next) =>  {
-  res.render('index', { title: 'Home', message: ""});
-});
+router.get('/home', indexController.displayHomePage);
 
 /* GET About Us page. */
-router.get('/about', (req, res, next) =>  {
-  res.render('index', { title: 'About', message: ""});
-});
+router.get('/about', indexController.displayAboutPage);
 
 /* GET Products page. */
-router.get('/products', (req, res, next) =>  {
-  res.render('index', { title: 'Products', message: ""});
-});
+router.get('/products', indexController.displayProductsPage);
 
 /* GET Services page. */
-router.get('/services', (req, res, next) =>  {
-  res.render('index', { title: 'Services', message: ""});
-});
+router.get('/services', indexController.displayServicesPage);
 
 /* GET Contact Us page. */
-// Use a different template as per 1.i.
-
-router.get('/contact', (req, res, next) =>  {
-  res.render('contact', { title: 'Contact', message: ""});
-});
+router.get('/contact', indexController.displayContactUsPage);
 
 /* POST Contact Us page. */
-router.post('/contact', (req, res, next) =>  {
-
-  // Grab the fields from the post
-  let postFirst = req.body.first;
-  let postLast = req.body.last;
-  let postEmail = req.body.email;
-  let postMessage = req.body.message;
-
-  // Demonstrating how the fields can be stored in JSON (event. a database) 
-  let formObject = {
-    firstName: postFirst,
-    lastName: postLast,
-    email : postEmail,
-    message: postMessage
-  };
-  console.log(formObject);
-
-  // Send a parameter with the redirect, to display message  
-  param1 = "thankyou"
-
-  res.redirect(`/?param1=${param1}`);
-});
-
+router.post('/contact', indexController.postContactUsPage);
 
 module.exports = router;
