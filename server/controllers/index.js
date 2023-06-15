d7f9bd69b8601ac476d9904170f5677b1c29e260
deschi0000 <79345657+deschi0000@users.noreply.github.com>
2023-06-15 10:32:23 +0800

    login/register/logout controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,12 @@
 // Dave von Deschwanden - 301303220 - 06.2023
 let express = require('express');
 let router = express.Router();
+let mongoose = require('mongoose');
+let passport = require('passport');
+
+// create the User Model Instance
+let userModel = require('../models/user');
+let User = userModel.User;  // alias
 
 /* GET Home page. */
 module.exports.displayHomePage = (req, res, next) => {
@@ -58,3 +64,98 @@ module.exports.postContactUsPage = (req, res, next) =>  {
   
     res.redirect(`/?param1=${param1}`);
   }
+
+module.exports.displayLoginPage = (req, res, next) => {
+    // check if the user is already logged in
+    if(!req.user){
+        res.render('auth/login', {
+            title: "Login",
+            messages: req.flash('loginMessage'),
+            displayName: req.user ? req.user.displayName : ''
+        });
+
+    } else {
+        return res.redirect('/');
+    }
+}
+
+module.exports.processLoginPage = (req, res, next) => {
+    passport.authenticate('local',
+    (err, user, info) => {
+        // server err?
+        if(err) {
+            return next(err);
+        }
+        // user login err?
+        if(!user){
+            req.flash('loginMessage', 'Authentication Error');
+            return res.redirect('/login');
+        }
+        // no issues~~
+        req.login(user, (err) => {
+            // check for error again (server disconnect/error/etc)
+            if(err) {
+                return next(err);
+            }
+            return res.redirect('/contact-list');
+        });
+    })(req, res, next);
+}
+
+module.exports.displayRegisterPage = (req, res, next) => {
+    // check if the user is not already logged in
+    if(!req.user){
+        res.render('auth/register', {
+            title: "Register",
+            messages: req.flash('registerMessage'),
+            displayName: req.user ? req.user.displayName : ''
+        });
+
+    } else {
+        return res.redirect('/');
+    }
+}
+
+module.exports.processRegisterPage = (req, res, next) => {
+    // initialize a user object
+    let newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        displayName: req.body.displayName
+    });
+
+    User.register(newUser, req.body.passport, (err) => {
+        if (err) {
+            console.log(err);
+            if (err.name == 'UserExistsError') {
+                req.flash(
+                    'registerMessage',
+                    'Registration Error: User Already Exists!'
+                ); 
+                console.log("Error: User Already Exists!");
+            }
+            return res.render('auth/register',
+            {
+                title:"Register",
+                messages: req.flash('registerMessage'),
+                displayName: req.user ? req.user.displayName : ''
+            });
+        } else {
+            // if registration is successful
+            return passport.authenticate('local')(req, res, () => {
+                res.redirect('/contact-list')
+            });
+        }
+    });
+}
+
+module.exports.performLogout = (req, res, next) => {
+    req.logout((err) => {
+        if (err){
+            // handle err
+            console.log(err);
+            return next(err);
+        }
+        return res.redirect('/');
+    });
+}
