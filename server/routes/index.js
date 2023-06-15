5839ef75ccbbe524142ae5cf099036aca15b95b3
deschi0000 <79345657+deschi0000@users.noreply.github.com>
2023-06-15 10:32:31 +0800

    login/register/logout routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,25 +5,43 @@ let router = express.Router();
 // Get the Controller
 let indexController = require('../controllers/index');
   
-/* GET Home page. */
+
+// GET Home page.
 router.get('/', indexController.displayHomePage);
 
-/* GET Home page. */
+// GET Home page.
 router.get('/home', indexController.displayHomePage);
 
-/* GET About Us page. */
+// GET About Us page.
 router.get('/about', indexController.displayAboutPage);
 
-/* GET Products page. */
+// GET Products page.
 router.get('/products', indexController.displayProductsPage);
 
-/* GET Services page. */
+// GET Services page.
 router.get('/services', indexController.displayServicesPage);
 
-/* GET Contact Us page. */
+// GET Contact Us page.
 router.get('/contact', indexController.displayContactUsPage);
 
-/* POST Contact Us page. */
+// POST Contact Us page. 
 router.post('/contact', indexController.postContactUsPage);
+
+
+
+// GET Route for displaying the Login Page
+router.get('/login', indexController.displayLoginPage);
+
+// POST Route for processing the Login Page
+router.post('/login', indexController.processLoginPage);
+
+// GET Route for displaying the Register Page
+router.get('/register', indexController.displayRegisterPage);
+
+// POST Route for displaying the Register Page
+router.post('/register', indexController.processRegisterPage);
+
+// GET to perform UserLogout
+router.get('/logout', indexController.performLogout);
 
 module.exports = router;
